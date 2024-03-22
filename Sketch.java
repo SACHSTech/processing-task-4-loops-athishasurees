9dac1080f89f7a969f1b8cd05446c76a5ae98dfd
athishasurees <athisha.surees26@ycdsbk12.ca>
2024-03-22 11:19:07 -0400

    done

--- Sketch.java
+++ Sketch.java
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255,255,255);
   }
 
   /**
@@ -24,13 +24,61 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+// first quadrant (grid) 
+  for(int lineX = 15; lineX <= 200; lineX += 15){
+    stroke(0,0,0);
+    line(lineX,0, lineX,200);
+   }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+  for(int lineY = 15; lineY <= 200; lineY += 15){
+      stroke(0,0,0);
+      line(0,lineY,200,lineY);
+    }
+
+// quadrant 2 (circles)
+    for (int circleY = 30; circleY <= 200; circleY += 35) {
+      for (int circleX = 230; circleX <= 400; circleX += 35) {
+        fill(212,15,125 );
+        ellipse(circleX, circleY, 20, 20);
+        }
+      }
+    
+// quadrant 3 (gradient)
+     double colours = 0; 
+     for (int lineX = 0; lineX <= 200; lineX ++){
+      line(lineX, 200, lineX, 400); 
+      colours += 1.25; 
+      stroke((int)colours, (int)colours, (int)colours);
+      
+     }
+
+
+// draw flower
+  fill(220, 93, 52);
+  stroke(0, 0, 0);
+  pushMatrix(); 
+  translate(300, 300); 
+        
+// draw petals
+  for (int i = 0; i < 9; i++) 
+      { 
+    rotate(PI / 4); 
+
+// Rotate by 45 degrees for each petal
+  ellipse(0, 0, 125, 20); 
+         
+// Draw petal 
+  } 
+  popMatrix();
+          
+  // Draw middle part of the flower
+    fill(38, 108, 45); 
+    ellipse(300, 300, 40, 40);
+
+    
+}
+}
+
+
   
   // define other methods down here.
-}
